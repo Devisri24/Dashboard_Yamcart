fe58cb29c92923a01fd2a096dc8d1fdf52014efb
Devisri <yennidevisri13.7@gmail.com>
2025-09-03 06:19:37 +0530

    added img on welcom

--- src/vendorDashboard/components/Welcome.jsx
+++ src/vendorDashboard/components/Welcome.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Welcome = () => {
-  return (
-    <div>
-        <h2>Welcome</h2>
+//   return (
+//     <div>
+//         <h2>Welcome</h2>
+//     </div>
+//   )
+// }
+  const [firmName, setFirmName] = useState("");
+ return (
+    <div className='welcomeSection'>
+        <h2>Welcome {firmName}</h2>
+        <div className="landingImage">
+          <img src='/assets/chef.jpg' alt='welcome' />
+        </div>
     </div>
   )
 }
-
 export default Welcome
